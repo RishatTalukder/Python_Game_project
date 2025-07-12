3c5b38d793dbe15cf57fe4d4b9b9e7b9255bab69
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-07-12 15:44:18 +0600

    Add initial settings configuration with default dimensions and background color

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pygame
+from settings import Settings
+
+class Ship:
+    def __init__(self, screen):
+        ship_image = pygame.image.load('./images/spaceship.png')
+        # self.ship_image = pygame.transform.scale(ship_image, (40,200))
+        self.ship_image = pygame.transform.scale_by(ship_image, .07)
+        self.screen = screen
+        
+    def blitme(self):    
+        self.screen.blit(self.ship_image, (100,200))
+
 
 class Main:
 
@@ -6,10 +18,27 @@ class Main:
 
         #pygame is a wrapper for SDL
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(size=(1200,800))
+        self.screen = pygame.display.set_mode(size=(
+            self.settings.width,
+            self.settings.height
+        ))
+        pygame.display.set_caption('Alien Invasion')
+
+        self.ship = Ship(self.screen)
+
+
+
+
+        #talk about this later
+        self.clock = pygame.time.Clock()
+        # Font for FPS
+        self.font = pygame.font.SysFont("Arial", 24)
 
     def run_game(self):
+        x=100
+        y = 100
         while True:
             events = pygame.event.get()
             print(events)
@@ -18,7 +47,26 @@ class Main:
                     pygame.quit()
                     return 
                 
+            self.screen.fill(self.settings.bg_color) #this is the backaground
+            self.render_fps(self.screen, self.clock, self.font)
+            self.ship.blitme()
+
+
             pygame.display.flip()
+
+
+            #talk about this later
+            self.clock.tick()
+
+    
+    #talk about this later
+    def render_fps(self,screen, clock, font):
+        fps = int(clock.get_fps())
+        fps_text = font.render(f"fps_rate: {fps}", True, (255, 0, 0))
+        screen.blit(fps_text, (10, 10))  # Top-left corner
+
+
+
 
 
 main = Main()
