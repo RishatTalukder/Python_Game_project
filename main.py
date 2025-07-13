c0f222817e57090ac7c8cd572ea1f0e47ec6239c
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-07-13 23:37:53 +0600

    Implement movement controls for the ship and update settings for ship speed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from math import floor,ceil
+from sys import exit
 
 
 class Main:
@@ -21,6 +23,14 @@ class Main:
 
 
 
+        #movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+
+
 
         #talk about this later
         self.clock = pygame.time.Clock()
@@ -28,29 +38,80 @@ class Main:
         self.font = pygame.font.SysFont("Arial", 24)
 
     def run_game(self):
-        x=100
-        y = 100
         while True:
-            events = pygame.event.get()
-            print(events)
-            for event in events:
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    return
-                elif event.type == pygame.KEYDOWN: 
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.ship_rect.x += 10
-                
-            self.screen.fill(self.settings.bg_color) #this is the backaground
-            self.render_fps(self.screen, self.clock, self.font)
-            self.ship.blitme()
+            self.check_events()
+            self.render()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            
+            #flagging
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    self.moving_left = True
+
+                elif event.key == pygame.K_UP:
+                    self.moving_up = True
+
+                elif event.key == pygame.K_DOWN:
+                    self.moving_down = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.moving_left = False
+
+                elif event.key == pygame.K_UP:
+                    self.moving_up = False
+
+                elif event.key == pygame.K_DOWN:
+                    self.moving_down = False
+            
+            
+
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_UP]:
+        #     self.ship.ship_rect.y -= 2
+        # if keys[pygame.K_DOWN]:
+        #     self.ship.ship_rect.y += 2
+        # if keys[pygame.K_LEFT]:
+        #     self.ship.ship_rect.x -= 2
+        # if keys[pygame.K_RIGHT]:
+        #     self.ship.ship_rect.x += 2
 
 
-            pygame.display.flip()
+    def render(self):
+        self.screen.fill(self.settings.bg_color) #this is the backaground
+        self.render_fps(self.screen, self.clock, self.font)
+        self.render_ship()
+
+        
+
+
+        pygame.display.flip()
 
 
             #talk about this later
-            self.clock.tick()
+        self.clock.tick()
+
+    def render_ship(self):
+        if self.moving_right == True:
+            self.ship.ship_rect.x += self.settings.ship_speed
+        if self.moving_left == True:
+            self.ship.ship_rect.x -= self.settings.ship_speed
+        if self.moving_up == True:
+            self.ship.ship_rect.y -= self.settings.ship_speed
+        if self.moving_down == True:
+            self.ship.ship_rect.y += self.settings.ship_speed
+        self.ship.blitme()
 
     
     #talk about this later
--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ class Settings:
         self.height = 800
         self.width = 1200
         self.bg_color = (200, 200, 200)
+        self.ship_speed = .5
